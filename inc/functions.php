96527a76f1f59df35fbfab1380ccec6e5a1db3e6
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-11-06 09:06:09 +0100

    phpapp

--- inc/functions.php
+++ inc/functions.php
@@ -1,33 +1,46 @@
 <?php
+
 // $pages = array(
 // 	'witam' => 'Witamy',
 // 	'formularz' => 'Formularz',
-// 	'klasa' => 'Klasa'
+// 	'klasa' => 'Klasy'
 // );
-function get_menu($id){
-	global $db;
-	$ret = array();
+
+// github.com/lo1cgsan/phpapp
+// tablica asocjcyjna, która będzie zawierała wyniki zapytań
+$ret = array();
+
+function get_menu($id) {
+	global $db, $ret;
 	db_query('SELECT * FROM menu', $ret);
-	// print_r($ret);
-	foreach ($ret as $k => $t) {
-		echo'
-			<li class="nav-item">
-              <a class="nav-link" href="?id='.$t['plik'].'">'.$t['tytul'].'</a>
-          </li>
-				';
+	//print_r($ret);
+ 	foreach ($ret as $k => $t) {
+		echo '
+<li class="nav-item">
+    <a class="nav-link" href="?id='.$t['plik'].'">'.$t['tytul'].'</a>
+</li>
+		';
 	}
 }
-function get_page_title($id){
-	global $pages;
-	if (array_key_exists($id, $pages))
-		echo $pages[$id];
+
+function get_page_title($id) {
+	global $ret;
+	foreach ($ret as $k => $t) {
+		//echo $t['id']." ";
+		if ($t['plik'] == $id) {
+			echo $t['tytul'];
+			return;
+		}
+	}
+	// tytuł domyślny
+	echo 'Aplikacja PHP';
+}
+
+function get_page_content($id) {
+	if (file_exists($id.'.html'))
+		include($id.'.html');
 	else
-		echo 'Aplikacja PHP';
+		include('404.html');
 }
-function get_page_content($id){
-if (file_exists($id.'.html'))
-	include($id.'.html');
-else
-	include('404.html');
-}
+
 ?>
